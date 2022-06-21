88d0ad9942e1be16dcb417caf1a59cff04d1a0c4
shashavalidudekula <shashavali.d7869@gmail.com>
2022-06-21 09:48:22 +0530

    listOrgRepos.sh

---
+++ listOrgRepos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Purpose: Bash Scripting Training
+#Author: Shashavali
+#Date: 21 June,2022
+#Use: List the repos in org
+
+
+curl \
+  -H "Accept: application/vnd.github.v3+json" \ 
+  -H "Authorization: token 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" \
+  https://api.github.com/orgs/ShashaDevopsTraining/repos
