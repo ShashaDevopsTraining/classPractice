eba3e792437c62dc7129edc34f5979a814b377d6
shashavalidudekula <shashavali.d7869@gmail.com>
2022-06-21 15:59:37 +0530

    updated PAT key

--- listOrgRepos.sh
+++ listOrgRepos.sh
@@ -4,7 +4,6 @@
 #Date: 21 June,2022
 #Use: List the repos in org
 
-
 curl \
   -H "Accept: application/vnd.github.v3+json" \ 
   -H "Authorization: token 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" \
