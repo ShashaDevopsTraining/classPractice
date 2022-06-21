39abf9be2bfa5be51fae19127404a512a493b241
Shashavali Dudekula <6346119+shashavalidudekula@users.noreply.github.com>
2022-06-21 18:56:20 +0530

    Update automateRepoActions.sh

--- automateRepoActions.sh
+++ automateRepoActions.sh
@@ -22,7 +22,7 @@ case $choice in
         echo "The existing repositories are ..."
         echo
         curl \
-            -s -S -H "Accept: application/vnd.github.v3+json" --user shashavalidudekula:<TOKEN> https://api.github.com/orgs/ShashaDevopsTraining/repos | grep -w "name" | awk '{print $2}' | grep -v repos | sed 's/"*"//g' | cut -d "," -f 1
+            -s -S -H "Accept: application/vnd.github.v3+json" --user <username>:<TOKEN> https://api.github.com/orgs/<ORG>/repos | grep -w "name" | awk '{print $2}' | grep -v repos | sed 's/"*"//g' | cut -d "," -f 1
         ;;
 
     2)
@@ -36,7 +36,7 @@ case $choice in
 
         #============= this will list all the repo names in given organization and redirect names to listOfRepo.txt file
         curl \
-        -s -S -H "Accept: application/vnd.github.v3+json" -H "Authorization: token <TOKEN>" https://api.github.com/orgs/ShashaDevopsTraining/repos | grep -w "name" | awk '{print $2}' | grep -v repos | sed 's/"*"//g' | cut -d "," -f 1 > listOfRepo.txt
+        -s -S -H "Accept: application/vnd.github.v3+json" -H "Authorization: token <TOKEN>" https://api.github.com/orgs/<ORG>/repos | grep -w "name" | awk '{print $2}' | grep -v repos | sed 's/"*"//g' | cut -d "," -f 1 > listOfRepo.txt
 
         #check the given repo name in the listOfRepo.txt file
         grep -ix $reponame listOfRepo.txt > /dev/null
@@ -51,8 +51,8 @@ case $choice in
             echo 
             curl \
             -X POST \
-            -s -S -H "Accept: application/vnd.github.v3+json" --user shashavalidudekula:<TOKEN> \
-            https://api.github.com/orgs/ShashaDevopsTraining/repos \
+            -s -S -H "Accept: application/vnd.github.v3+json" --user <username>:<TOKEN> \
+            https://api.github.com/orgs/<ORG>/repos \
             -d '{"name":"'$reponame'" ,"description":"This is your first repository","private":false}' > /dev/null
             echo
             echo "$reponame repo is created successfully."
@@ -70,7 +70,7 @@ case $choice in
 
         #============= this will list all the repo names in given organization and redirect names to listOfRepo.txt file
         curl \
-        -s -S -H "Accept: application/vnd.github.v3+json" --user shashavalidudekula:<TOKEN> https://api.github.com/orgs/ShashaDevopsTraining/repos | grep -w "name" | awk '{print $2}' | grep -v repos | sed 's/"*"//g' | cut -d "," -f 1 > listOfRepo.txt
+        -s -S -H "Accept: application/vnd.github.v3+json" --user <username>:<TOKEN> https://api.github.com/orgs/<ORG>/repos | grep -w "name" | awk '{print $2}' | grep -v repos | sed 's/"*"//g' | cut -d "," -f 1 > listOfRepo.txt
 
         #check the given repo name in the listOfRepo.txt file
         grep -ix $reponame listOfRepo.txt > /dev/null
@@ -81,7 +81,7 @@ case $choice in
             echo 
            curl \
             -X DELETE \
-            -s -S -H "Accept: application/vnd.github.v3+json" --user shashavalidudekula:<TOKEN> https://api.github.com/repos/ShashaDevopsTraining/$reponame
+            -s -S -H "Accept: application/vnd.github.v3+json" --user <username>:<TOKEN> https://api.github.com/repos/<ORG>/$reponame
             echo
             echo "$reponame repo is deleted successfully."
         else
